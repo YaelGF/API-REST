a20b2fd9719de63b2cc068b44600ca9a46984df0
YaelGF <1719110736@utectulancingo.edu.mx>
2022-06-11 03:50:23 +0000

    Ready

---
+++ code/main.py
@@ -0,0 +1,39 @@
+from typing import Union
+from fastapi import FastAPI
+import sqlite3
+from typing import List
+from pydantic import BaseModel
+
+class Respuesta(BaseModel):
+    message: str
+
+class Cliente(BaseModel):
+    id_cliente: int
+    nombre: str
+    email: str
+
+app = FastAPI()
+
+
+@app.get("/", response_model=Respuesta)
+async def index():
+    return {"message": "Fast API"}
+
+@app.get("/clientes/", response_model=List[Cliente])
+async def read_root():
+    with sqlite3.connect('sql/clientes.sqlite') as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM clientes')
+        response = cursor.fetchall()
+        return response
+
+        
+@app.get("/clientes/{id_cliente}", response_model=Cliente)
+async def clientes_parametros(id_cliente: int):
+    with sqlite3.connect("sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("Select * From clientes where id_cliente = {}".format(id_cliente))
+        response = cursor.fetchone()
+        return response
---
+++ docker/README.md
@@ -0,0 +1,51 @@
+# Config Ubuntu 20.04 and Python with FastAPI
+
+## 1. Dockefile 
+```
+FROM ubuntu:20.04
+LABEL description = "Tarea 2.1"
+RUN apt update
+RUN apt upgrade -y
+
+RUN apt-get install -y python3
+RUN apt-get install -y sqlite3
+RUN apt install -y python3-pip
+
+ENV requirements /home/requirements.txt
+COPY requirements.txt ${requirements}
+
+RUN pip3 install uvicorn==0.17.6
+RUN pip3 install fastapi==0.78.0
+RUN pip3 install pytest==7.1.1
+
+RUN pip3 install web.py==0.62
+
+RUN pip3 install black==22.3.0
+RUN pip3 install flake8==4.0.1
+RUN pip3 install isort==5.10.1
+RUN pip3 install mypy==0.961
+
+WORKDIR /home/
+```
+
+## 2. Build Docker image
+```
+docker build -t fastapi:v1 .
+```
+
+## 3. Create docker container
+nota:regresar a la carpeta principal
+```
+cd ..
+```
+
+```
+docker run -it -v $(pwd)/code:/PaginaFastAPI --net host --name fastapy -h python fast:0.1
+```
+
+## 4. Run Web Page
+
+```
+$ cd PaginaFastAPI
+$ uvicorn main:app --reload
+```
