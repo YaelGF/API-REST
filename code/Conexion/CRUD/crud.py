e800029bea79c80da0b23a40a79a8edb88e166d1
Yael Garcia <65678150+YaelGF@users.noreply.github.com>
2022-06-23 22:22:36 -0500

    May be

--- code/Conexion/CRUD/crud.py
+++ code/Conexion/CRUD/crud.py
@@ -1,6 +1,11 @@
+import email
+from sqlite3 import Cursor
 from .Conexion import conexion
 
-def Create():
+def Create(nombre:str,email:str):
+    cursor = conexion.OpenConexion()
+    cursor.execute( "INSERT INTO clientes(nombre, email) values('?','?')",
+    (nombre,email),)
 
 def Read(limit:int=10,offset:int=0):
     cursor = conexion.OpenConexion()
@@ -9,7 +14,20 @@ def Read(limit:int=10,offset:int=0):
     query = cursor.fetchall()
     return query
 
-def Update():
+def Update(nombre: str, email:str, id_cliente:int):
+    cursor = conexion.OpenConexion()
+    cursor.execute( 
+        "UPDATE clientes set nombre = '?', email = '?' WHERE id_cliente = ?",
+        (nombre,email,id_cliente),)
 
+def Delete(id_cliente:int):
+    cursor = conexion.OpenConexion()
+    cursor.execute( "DELETE FROM clientes Where id_cliente = ?",
+        (id_cliente),)
 
-def Delete():
+def Read_with_Params(id_cliente:int):
+    cursor = conexion.OpenConexion()
+    cursor.execute( "SELECT * FROM clientes Where id_cliente = ?",
+        (id_cliente),)
+    query = cursor.fetchone()
+    return query
--- code/main.py
+++ code/main.py
@@ -1,10 +1,8 @@
 from typing import Union
 from fastapi import FastAPI
-import sqlite3
 from typing import List
 from .Schemas import schemas
-
-
+from .Conexion.CRUD import crud
 
 app = FastAPI()
 
@@ -14,48 +12,26 @@ async def index():
 
 @app.get("/clientes/", response_model=List[schemas.Cliente])
 async def clientes(offset:int =0,limit: int = 10):
-    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM clientes  LIMIT {limit}  OFFSET {offset}'.format(offset=offset,limit=limit))
-        response = cursor.fetchall()
-        return response
+    return crud.Read()
 
 @app.get("/clientes/{id_cliente}", response_model=schemas.Cliente)
 async def cliente_parametros(id_cliente: int):
-    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM clientes where id_cliente = {}".format(id_cliente))
-        response = cursor.fetchone()
-        return response
+    return crud.Read_with_Params()
         
 @app.post("/clientes/", response_model=schemas.Mensaje)
 async def cliente_add(nombre:str,email:str):
-    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("insert into clientes(nombre,email) values('{nombre}', '{email}')".format(nombre=nombre, email=email))
-        response = cursor.fetchone()
-        data = {"mensaje":"Cliente agregado"}
-        return data
+    crud.Create(nombre,email)
+    data = {"mensaje":"Cliente agregado"}
+    return data
 
 @app.put("/clientes/{id_cliente}", response_model=schemas.Mensaje)
 async def cliente_put(id_cliente: int, nombre:str,email:str):
-    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("Update clientes set nombre = '{name}', email = '{email}' where id_cliente = {id}".format(name=nombre,email=email,id=id_cliente))
-        response = cursor.fetchone()
-        data = {"mensaje":"Cliente actualizado"}
-        return data
+    crud.Update(nombre,email,id_cliente)
+    data = {"mensaje":"Cliente actualizado"}
+    return data
 
 @app.delete("/clientes/{id_cliente}", response_model=schemas.Mensaje)
 async def cliente_delete(id_cliente: int):
-    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("Delete from clientes where id_cliente = {}".format(id_cliente))
-        response = cursor.fetchone()
-        data = {"mensaje":"Cliente borrado"}
-        return data
+    crud.Delete(id_cliente)
+    data = {"mensaje":"Cliente borrado"}
+    return data
