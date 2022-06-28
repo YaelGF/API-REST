33fb72a3d1c89ca0f33cbada4f36dbfd720a64ee
YaelGF <1719110736@utectulancingo.edu.mx>
2022-06-27 22:25:53 -0500

    Auth in backend READY

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
 import sqlite3
 from typing import List
-from pydantic import BaseModel
+import hashlib  # importa la libreria hashlib
+
 
 class Respuesta(BaseModel):
     message: str
@@ -22,58 +25,118 @@ class ClienteNew(BaseModel):
 class ClienteID(BaseModel):
     id_cliente: int
 
-
+class Usuarios(BaseModel):
+    username: str
+    level: int
 
 app = FastAPI()
+
+security = HTTPBasic()
+
+def get_current_level(credentials: HTTPBasicCredentials = Depends(security)):
+    password_b = hashlib.md5(credentials.password.encode())
+    password = password_b.hexdigest()
+    with sqlite3.connect('backend/sql/clientes.sqlite') as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT level FROM usuarios WHERE username = ? and password = ?",
+            (credentials.username, password),
+        )
+        user = cursor.fetchone()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+    return user[0]
 
 @app.get("/", response_model=Respuesta)
 async def index():
     return {"message": "Fast API"}
 
 @app.get("/clientes/", response_model=List[Cliente])
-async def clientes(offset:int =0,limit: int = 10):
-    with sqlite3.connect('backend/sql/clientes.sqlite') as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM clientes  LIMIT {limit}  OFFSET {offset}'.format(offset=offset,limit=limit))
-        response = cursor.fetchall()
-        return response
+async def clientes(offset:int =0,limit: int = 10,level: int = Depends(get_current_level)):
+    if level == 1:
+        with sqlite3.connect('backend/sql/clientes.sqlite') as connection:
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            cursor.execute('SELECT * FROM clientes  LIMIT {limit}  OFFSET {offset}'.format(offset=offset,limit=limit))
+            response = cursor.fetchall()
+            return response
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Don't have permission to access this resource",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 @app.get("/clientes/{id_cliente}", response_model=Cliente)
-async def cliente_parametros(id_cliente: int):
-    with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM clientes where id_cliente = {}".format(id_cliente))
-        response = cursor.fetchone()
-        return response
+async def cliente_parametros(id_cliente: int,level: int = Depends(get_current_level)):
+    if level == 1:    
+        with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM clientes where id_cliente = {}".format(id_cliente))
+            response = cursor.fetchone()
+            return response
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Don't have permission to access this resource",
+            headers={"WWW-Authenticate": "Basic"},
+        )
         
 @app.post("/clientes/", response_model=Mensaje)
-async def cliente_add(nombre:str,email:str):
-    with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("insert into clientes(nombre,email) values('{nombre}', '{email}')".format(nombre=nombre, email=email))
-        response = cursor.fetchone()
-        data = {"mensaje":"Cliente agregado"}
-        return data
+async def cliente_add(nombre:str,email:str,level: int = Depends(get_current_level)):
+    if level == 0:    
+        with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            cursor.execute("insert into clientes(nombre,email) values('{nombre}', '{email}')".format(nombre=nombre, email=email))
+            response = cursor.fetchone()
+            data = {"mensaje":"Cliente agregado"}
+            return data
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Don't have permission to access this resource",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 @app.put("/clientes/{id_cliente}", response_model=Mensaje)
-async def cliente_put(id_cliente: int, nombre:str,email:str):
-    with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("Update clientes set nombre = '{name}', email = '{email}' where id_cliente = {id}".format(name=nombre,email=email,id=id_cliente))
-        response = cursor.fetchone()
-        data = {"mensaje":"Cliente actualizado"}
-        return data
+async def cliente_put(id_cliente: int, nombre:str,email:str,level: int = Depends(get_current_level)):
+    if level == 0:    
+        with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            cursor.execute("Update clientes set nombre = '{name}', email = '{email}' where id_cliente = {id}".format(name=nombre,email=email,id=id_cliente))
+            response = cursor.fetchone()
+            data = {"mensaje":"Cliente actualizado"}
+            return data
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Don't have permission to access this resource",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 @app.delete("/clientes/{id_cliente}", response_model=Mensaje)
-async def cliente_delete(id_cliente: int):
-    with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("Delete from clientes where id_cliente = {}".format(id_cliente))
-        response = cursor.fetchone()
-        data = {"mensaje":"Cliente borrado"}
-        return data
+async def cliente_delete(id_cliente: int,level: int = Depends(get_current_level)):
+    if level == 0:  
+        with sqlite3.connect("backend/sql/clientes.sqlite") as connection:
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            cursor.execute("Delete from clientes where id_cliente = {}".format(id_cliente))
+            response = cursor.fetchone()
+            data = {"mensaje":"Cliente borrado"}
+            return data
+    
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Don't have permission to access this resource",
+            headers={"WWW-Authenticate": "Basic"},
+        )
--- backend/sql/clientes.sql
+++ backend/sql/clientes.sql
@@ -15,3 +15,17 @@ insert into clientes(nombre,email) values ("Mauricio","mau@email.com");
 .headers ON
 
 Select * From clientes;
+
+Drop table if exists usuarios;
+CREATE TABLE usuarios(
+    username TEXT,
+    password varchar(32),
+    level INTEGER
+);
+
+CREATE UNIQUE INDEX index_usuario ON usuarios(username);
+
+INSERT INTO usuarios(username, password, level) VALUES('admin','21232f297a57a5a743894a0e4a801fc3',0);
+INSERT INTO usuarios(username, password, level) VALUES('user','ee11cbb19052e40b07aac0ca060c23ee',1);
+
+SELECT * FROM usuarios;
